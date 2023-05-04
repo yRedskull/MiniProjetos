818b6e09df397ada2904b6c742773e93e9aa354f
T. Felipe <eusouored@hotmail.com>
2023-05-04 15:10:50 -0300

    Update main.js

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -2,7 +2,7 @@ class Calculadora {
     constructor() {
         this.display = document.querySelector('#calc')
         this.symbolsTextArray = ['C', '(', ')', "/", ',', '↩', '*', '+', '-', '=', 'Backspace']
-        this.symbolsIdObj = {dvs: "/", x: "*", plus: "+", minus: "-", dot: ",", equal: "Enter"}
+        this.symbolsKeysObj = {dvs: "/", x: "*", plus: "+", minus: "-", dot: ",", equal: "Enter"}
         this.arrowArray = ["ArrowUp", "ArrowRight", "ArrowDown",  "ArrowLeft"]
         this.numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
         this.resultTrue = false}
@@ -20,14 +20,14 @@ class Calculadora {
                     this.resultTrue = false
                     this.display.value = ''
                 } 
-                if (Object.values(this.symbolsIdObj).includes(e.key)){
+                if (Object.values(this.symbolsKeysObj).includes(e.key)){
                     this.resultTrue = false
                 }
             }
-            if (Object.values(this.symbolsIdObj).includes(e.key)) {
+            if (Object.values(this.symbolsKeysObj).includes(e.key)) {
                 if (this.display.value === '') this.display.blur()
-                for (let chave of Object.keys(this.symbolsIdObj)) {
-                    if (this.symbolsIdObj[chave] === e.key) {
+                for (let chave of Object.keys(this.symbolsKeysObj)) {
+                    if (this.symbolsKeysObj[chave] === e.key) {
                         const sym = document.getElementById(chave)
                         sym.classList.add("hover-btn")
                         setTimeout(() => {
@@ -55,9 +55,9 @@ class Calculadora {
         })
 
         this.display.addEventListener('keypress', (e) => {
-            if (Object.values(this.symbolsIdObj).includes(e.key) 
-                && Object.values(this.symbolsIdObj).includes(this.display.value.slice(-1))
-                || !Object.values(this.symbolsIdObj).includes(e.key)
+            if (Object.values(this.symbolsKeysObj).includes(e.key) 
+                && Object.values(this.symbolsKeysObj).includes(this.display.value.slice(-1))
+                || !Object.values(this.symbolsKeysObj).includes(e.key)
                 && !this.numArray.includes(e.key)) this.display.blur()
         })
     }
@@ -66,22 +66,29 @@ class Calculadora {
         document.addEventListener('click', (e) => {
             const el = e.target
             if (el.classList.contains('btn')){
-                if (el.innerText == 'C') this.clear() 
-                else if (el.innerText == '=') this.result() 
-                else if (el.innerText == '↩' ) this.del_case() 
-                else {
-                    if (this.resultTrue) {
-                        if (el.innerText in this.numArray) {
-                            this.resultTrue = false
-                            return this.display.value = el.innerText
-                        } else{
-                        this.display.value += el.innerText
+
+                if (el.innerText ==='C') return this.clear() 
+                if (el.innerText === '=') return this.result() 
+                if (el.innerText === '↩' ) return this.del_case() 
+
+                if (Object.values(this.symbolsTextArray).includes(el.innerText)){
+
+                    if (this.display.value === '' 
+                        || Object.values(this.symbolsKeysObj).includes(this.display.value.slice(-1))) return
+                }
+                        
+            
+                if (this.resultTrue) {
+                    if (el.innerText in this.numArray) {
                         this.resultTrue = false
-                    }
-                    } else {
-                        this.display.value += el.innerText
-                    }
-                    }
+                        return this.display.value = el.innerText
+                    } else{
+                    this.display.value += el.innerText
+                    this.resultTrue = false
+                }} else {
+                    this.display.value += el.innerText
+                }
+                    
             }
 
             
